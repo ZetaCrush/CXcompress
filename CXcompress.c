a09e74e59900b75cd458d4d6cb383a6968989e84
seccode <42663376+seccode@users.noreply.github.com>
2025-05-05 13:36:27 -0600

    Add files via upload

--- CXcompress.c
+++ CXcompress.c
@@ -31,9 +31,8 @@ DictEntry* load_dictionary(const char* dict_path, const char* lang_path, size_t*
     size_t i = 0;
 
     while (fgets(dict_line, MAX_LINE, dict_file) && fgets(lang_line, MAX_LINE, lang_file)) {
-        // Remove newline
-        dict_line[strcspn(dict_line, "\r\n")] = 0;
-        lang_line[strcspn(lang_line, "\r\n")] = 0;
+        dict_line[strcspn(dict_line, "\n")] = 0;
+        lang_line[strcspn(lang_line, "\n")] = 0;
 
         entries[i].word = strdup(dict_line);
         entries[i].symbol = strdup(lang_line);
@@ -69,15 +68,22 @@ char find_unused_char_from_buffer(const char* buffer, size_t len) {
     exit(1);
 }
 
-void compress(const char* dict_path, const char* lang_path, const char* input_buffer, size_t input_len) {
-    printf("Running compression setup...\n");
+void compress(const char* dict_path, const char* lang_path, const char* input_buffer, size_t input_len, int threads) {
 
     size_t dict_size = 0;
     DictEntry* dict = load_dictionary(dict_path, lang_path, &dict_size);
 
-    printf("Loaded %zu dictionary entries.\n", dict_size);
-
     char escape_char = find_unused_char_from_buffer(input_buffer, input_len);
+
+    free_dictionary(dict, dict_size);
+}
+
+void decompress(const char* dict_path, const char* lang_path, const char* input_buffer, size_t input_len, int threads) {
+
+    size_t dict_size = 0;
+    DictEntry* dict = load_dictionary(dict_path, lang_path, &dict_size);
+
+    char escape_char = input_buffer[0];
 
     free_dictionary(dict, dict_size);
 }
@@ -101,17 +107,15 @@ char* read_file(const char* path, const char* label, size_t* out_len) {
     }
 
     size_t read_bytes = fread(buffer, 1, length, file);
-    buffer[read_bytes] = '\0';
 
     fclose(file);
-    printf("%s read: %zu bytes\n", label, read_bytes);
     if (out_len) *out_len = read_bytes;
     return buffer;
 }
 
 int main(int argc, char* argv[]) {
-    if (argc != 5) {
-        fprintf(stderr, "Usage: %s <-c|-d> <file_path> <dictionary_file_path> <language_file_path>\n", argv[0]);
+    if (argc != 6) {
+        fprintf(stderr, "Usage: %s <-c|-d> <file_path> <dictionary_file_path> <language_file_path> <thread_count>\n", argv[0]);
         return 1;
     }
 
@@ -119,21 +123,18 @@ int main(int argc, char* argv[]) {
     const char* file_path = argv[2];
     const char* dict_path = argv[3];
     const char* language_path = argv[4];
+    int threads = atoi(argv[5]);
 
     if (strcmp(mode_flag, "-c") == 0) {
-        printf("Mode: Compression\n");
         size_t input_len = 0;
         char* input_buffer = read_file(file_path, "Input file", &input_len);
-        compress(dict_path, language_path, input_buffer, input_len);
+        compress(dict_path, language_path, input_buffer, input_len, threads);
         free(input_buffer);
     } else if (strcmp(mode_flag, "-d") == 0) {
-        printf("Mode: Decompression (not yet implemented)\n");
-        size_t input_len0 = 0;
-        size_t input_len1 = 0;
-        size_t input_len2 = 0;
-        read_file(file_path, "Input file", &input_len0);
-        read_file(dict_path, "Dictionary", &input_len1);
-        read_file(language_path, "Language pack", &input_len2);
+        size_t input_len = 0;
+        char* input_buffer = read_file(file_path, "Input file", &input_len);
+        decompress(dict_path, language_path, input_buffer, input_len, threads);
+        free(input_buffer);
     } else {
         fprintf(stderr, "Invalid mode: %s. Use -c for compression or -d for decompression.\n", mode_flag);
         return 1;
